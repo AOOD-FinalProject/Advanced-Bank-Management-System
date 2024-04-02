23ce4b428505f90b298649a490850af88da12d9c
Michael Helzer <michaelhelzer1@gmail.com>
2024-04-02 17:16:53 -0500

    new files added
    Added Person class that inherits both user and manager classes.

--- User.h
+++ User.h
@@ -2,18 +2,18 @@
 
 #ifndef FINAL_PROJECT_USER_H
 #define FINAL_PROJECT_USER_H
-
+#include <string>
 
 class User{
 public:
     void create_User();
     void remove_User();
-    void login_User();
+    void login_User(string userName, string passWord);
     int withdraw(int withdraw_amount);
     int deposit(int deposit_amount);
 
 private:
-    int get_account_value();
+    //int get_account_value();
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,41 +1,48 @@
 
-#include "User.h"
+#include "Person.h"
 #include <iostream>
+#include <string>
+#include <cstdlib> // for exit()
 using namespace std;
 
 int main() {
 
-// prompt initial login
-cout << "\nWelcome To Your Banking System" << endl;
-cout << "\t1. User Login" << endl;
-cout << "\t2. Create Account" << endl;
-cout << "\t3. Manager Login" << endl;
-cout << "\t4. Exit\n" << endl;
 
-int ans;
-cin >> ans;
+    cout << "\nWelcome To Your Banking System" << endl;
+    cout << "\t1. User Login" << endl;
+    cout << "\t2. Create Account" << endl;
+    cout << "\t3. Manager Login" << endl;
+    cout << "\t4. Exit\n" << endl;
 
-if (ans == 1) {
-    cout << "Enter Username: " << endl;
-    string userName;
-    cin >> userName;
-    cout << "Enter Password: " << endl;
-    string passWord;
-    cin >> passWord;
-    void User::login_User(userName, passWord);
-}
+    int ans;
+    cin >> ans;
 
-// show list of options
+    // Call the appropriate function based on user input
+    Person p;
+    switch (ans) {
+        case 1:
+            cout << "Enter Username: " << endl;
+            string userName;
+            cin >> userName;
+            cout << "Enter Password: " << endl;
+            string passWord;
+            cin >> passWord;
+            p.user_login_user(userName, passWord);
+            break;
+        case 2:
+            p.user_create_user();
+            break;
+        case 3:
+            p.manager_login_manager();
+            break;
+        case 4:
+            cout << "Exiting the program." << endl;
+            return 0;
+            break;
+        default:
+            cout << "Invalid choice. Please enter a number between 1 and 4." << std::endl;
+            break;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 }
