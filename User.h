1cdda50396fd8d31c1116f00ac6ec7a018415415
Michael Helzer <michaelhelzer1@gmail.com>
2024-04-10 13:21:52 -0500

    fixed my code

--- User.h
+++ User.h
@@ -1,35 +1,33 @@
 #ifndef ADVANCED_BANK_MANAGEMENT_SYSTEM_PERSON_H
 #define ADVANCED_BANK_MANAGEMENT_SYSTEM_PERSON_H
 #include <iostream>
-#include <fstream>//I/O files
+#include <fstream>
 #include <string>
-#include <iomanip>//setprecision()
-#include <vector>//vectors better than arrays
-
+#include <cctype>
 using namespace std;
-
-//keep here or move?
-bool isAlphaNumeric(string input);
-bool isTwoDecimalPlaces(double input);
 
 class User {
 private:
-	//any private?
+    float balance;
+    static int accountNumber;
 public:
-	fstream TextFile;
-	fstream UsersTextFile;
-	ofstream temp;
-	//string use to read file
-	string username, user, pass, line;
-	bool matches;
-
-	User();
-	//copy constructor?
-	~User();
-	virtual int login(string userName, string passWord);
-	string createUser();
-	int userExist(string userName);
-	void removeUser(string userName);
+    fstream UsersTextFile;
+    ofstream temp;
+    string user;
+    string pass;
+    string accNum;
+    string Name;
+    string accountType;
+    string bal;
+    string line;
+    bool match = false;
+    bool menu = true;
+    int ans;
+    static int activeAccounts;
+    User();
+    ~User();
+    virtual void login(string userName, string passWord);
+    void create();
 };
 
 #endif //ADVANCED_BANK_MANAGEMENT_SYSTEM_PERSON_H
--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include "User.h"
 #include "Manager.h"
-#include "BankAccount.h"
-#include <cstdlib> // for exit()?
+#include "Transaction.h"
+#include <cstdlib> // for exit()
 
 int main() {
     //initialize variables
     int ans;
-    bool mainMenu = true;
+    bool menu = true;
     string userName;
     string passWord;
-    BankAccount* bankAccount;
     User* user;
 
     //do while loop to validate input
@@ -24,79 +23,42 @@ int main() {
 
         //case base on input
         switch (ans) {
-        //user login case
-        case 1:
-            user = new User();
-            cout << "Enter Username: " << endl;
-            cin >> userName;
-            cout << "Enter Password: " << endl;
-            cin >> passWord;
-            if (user->login(userName, passWord) == 1) {
-                bankAccount = new BankAccount();
-                bankAccount->loadBankAccount(userName);
-                delete bankAccount;
-            }
-            delete user;
-            break;
-        //create user login case
-        case 2:
-            user = new User();
-            bankAccount = new BankAccount();
-            bankAccount->addAccount(user->createUser());
-            delete bankAccount;
-            delete user;
-            break;
-        //manager login case
-        case 3:
-            user = new Manager();
-            cout << "Enter Username: " << endl;
-            cin >> userName;
-            cout << "Enter Password: " << endl;
-            cin >> passWord;
-            user->login(userName, passWord);
-            delete user;
-            break;
-            //exit case
-        case 4:
-            cout << "Exiting the program." << endl;
-            return 0;
-        //default case
-        default:
-            //clear error input due to input type doesn't match and ignore input
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Invalid choice. Please enter a number between 1 and 4.\n" << endl;
+            //user login case
+            case 1:
+                user = new User();
+                cout << "Enter Username: " << endl;
+                cin >> userName;
+                cout << "Enter Password: " << endl;
+                cin >> passWord;
+                user->login(userName, passWord);
+                delete user;
+                break;
+                //create user login case
+            case 2:
+                user = new User();
+                user->create();
+                delete user;
+                break;
+                //manager login case
+            case 3:
+                user = new Manager();
+                cout << "Enter Username: " << endl;
+                cin >> userName;
+                cout << "Enter Password: " << endl;
+                cin >> passWord;
+                user->login(userName, passWord);
+                delete user;
+                break;
+                //exit case
+            case 4:
+                cout << "Exiting the program." << endl;
+                return 0;
+                //default case
+            default:
+                //clear error input due to input type doesn't match and ignore input
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid choice. Please enter a number between 1 and 4.\n" << endl;
         }
-    } while (mainMenu == true);
-    return 0;
-}
-
-//keep here or move?
-//function to take only letters and numbers as input
-bool isAlphaNumeric(string input) {
-    for (char c : input) {
-        if (!isalnum(c)) {
-            cout << "Enter only letters and numbers. Try again.\n" << endl;
-            return false;
-        }
-    }
-    return true;
-}
-
-//keep here or move?
-//function to take only numbers and if it is a decimal number no more than 2 decimal places
-bool isTwoDecimalPlaces(double input) {
-    if (input <= 0) {
-        //clear error input due to input type doesn't match and ignore input
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Enter a number greater than 0. Try again.\n" << endl;
-        return false;
-    }
-    double roundedInput = round(input * 100) / 100;
-    if (input != roundedInput) {
-        cout << "Enter a number with 2 decimal places. Try again.\n" << endl;
-        return false;
-    }
-    return true;
+    } while (menu == true);
 }
